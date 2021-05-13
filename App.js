e132f807b62683a800b3fa66a807fe3dfd569eb5
GBR\gteng <tengelicsgabor@gmail.com>
2021-05-13 14:24:02 +0200

    draggability with needed packages

--- App.js
+++ App.js
@@ -7,7 +7,11 @@ import {
   Text,
   View,
   FlatList,
+  TouchableOpacity,
 } from 'react-native';
+import DraggableFlatList, {
+  RenderItemParams,
+} from 'react-native-draggable-flatlist';
 import {dummyTasks} from './dummyTasks';
 
 class App extends Component {
@@ -16,6 +20,11 @@ class App extends Component {
   };
   onViewableItemsChanged = ({viewableItems}) => {
     console.log('First visible index: ', viewableItems[0].index);
+  };
+  reorderItems = newArray => {
+    this.setState({
+      todos: newArray,
+    });
   };
   render() {
     console.log('this.state: ', this.state);
@@ -29,11 +38,12 @@ class App extends Component {
             backgroundColor: 'lightgray',
             flex: 0.3,
           }}></View>
-        <FlatList
+        <DraggableFlatList
           onViewableItemsChanged={this.onViewableItemsChanged}
           data={this.state.todos}
           renderItem={this.renderItem}
-          keyExtractor={(item, index) => index}
+          keyExtractor={(item, index) => `draggable-item-${index}`}
+          onDragEnd={({data}) => this.reorderItems(data)}
           style={{
             flex: 1,
           }}
@@ -41,10 +51,26 @@ class App extends Component {
       </SafeAreaView>
     );
   }
-  renderItem = ({item}) => (
-    <View style={{height: 50}}>
-      <Text style={{}}>{item.taskName}</Text>
-    </View>
+  renderItem = ({item, index, drag, isActive}) => (
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomWidth: 0.3,
+        borderBottomColor: 'black',
+      }}
+      onLongPress={drag}
+      activeOpacity={1}
+      onPress={() => {
+        console.log('pressed');
+      }}>
+      <Text
+        style={{
+          paddingVertical: 14,
+        }}>
+        {item.taskName}
+      </Text>
+    </TouchableOpacity>
   );
 }
 
