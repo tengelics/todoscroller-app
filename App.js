7a8af0a73844335af036640662a7e318be293a31
GBR\gteng <tengelicsgabor@gmail.com>
2021-05-13 14:03:33 +0200

    basic flatlist with scroll listener

--- App.js
+++ App.js
@@ -1,112 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  FlatList,
 } from 'react-native';
+import {dummyTasks} from './dummyTasks';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+class App extends Component {
+  state = {
+    todos: dummyTasks,
   };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+  onViewableItemsChanged = ({viewableItems}) => {
+    console.log('First visible index: ', viewableItems[0].index);
+  };
+  render() {
+    console.log('this.state: ', this.state);
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}>
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+            backgroundColor: 'lightgray',
+            flex: 0.3,
+          }}></View>
+        <FlatList
+          onViewableItemsChanged={this.onViewableItemsChanged}
+          data={this.state.todos}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => index}
+          style={{
+            flex: 1,
+          }}
+        />
+      </SafeAreaView>
+    );
+  }
+  renderItem = ({item}) => (
+    <View style={{height: 50}}>
+      <Text style={{}}>{item.taskName}</Text>
+    </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ dummyTasks.js
@@ -0,0 +1,146 @@
+export const dummyTasks = [
+  {
+    taskName: 'HelloTaskFirst',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3',
+    hardness: 1,
+  },
+  {
+    taskName: 'HelloTask',
+    hardness: 5,
+  },
+  {
+    taskName: 'HelloTask2',
+    hardness: 2,
+  },
+  {
+    taskName: 'HelloTask3last',
+    hardness: 1,
+  },
+];
