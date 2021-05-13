8d25d1339ebaeda1a8438151090d90176ee1bf4b
GBR\gteng <tengelicsgabor@gmail.com>
2021-05-13 13:35:54 +0200

    initial commit

---
+++ android/app/src/main/java/com/todoscroller_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.todoscroller_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "todoscroller_app";
+  }
+}
